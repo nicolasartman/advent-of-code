67e787917fb505c367f21150ad7d87b163e186b3
Nicolas Artman <nicolasartman@gmail.com>
2015-12-13 22:28:50 -0800

    Add day 3 bootstrapping

--- 2.rkt
+++ 2.rkt
@@ -2,12 +2,27 @@
 
 (require 2htdp/batch-io)
 
+;; first part
+
 (define (calc-paper-needed l w h)
   (let ([side-areas (list (* l w) (* w h) (* h l))])
     (+ (apply min side-areas)
        (* 2 (apply + side-areas)))))
 
+(define (calc-ribbon-needed l w h)
+  (let ([wrap-length (* 2
+                        (let* ([smallest-dim (min l w h)]
+                               [second-smallest-dim (apply min (remove smallest-dim (list l w h)))])
+                          (+ smallest-dim second-smallest-dim)))]
+        [bow-length (* l w h)])
+    (+ wrap-length bow-length)))
+
 (let ([package-dimensions (map (lambda (dims) (map string->number (string-split dims "x")))
                                (string-split (read-file "2-input.txt")))])
-  (print package-dimensions)
-  (apply + (map (lambda (dims) (apply calc-paper-needed dims)) package-dimensions)))
+  (let ([result-pairs (map (lambda (dims) (list
+                                           (apply calc-paper-needed dims)
+                                           (apply calc-ribbon-needed dims)))
+                           package-dimensions)])
+    (map (lambda (result-pair-getter)
+           (foldl (lambda (result-pair sum) (+ sum (result-pair-getter result-pair))) 0 result-pairs))
+         (list first second))))
