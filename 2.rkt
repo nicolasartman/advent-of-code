8d30159b3f27534cd78383383180015f533945ff
Nicolas Artman <nicolasartman@gmail.com>
2015-12-13 22:15:44 -0800

    Add day 2 solutions

--- 2.rkt
+++ 2.rkt
@@ -1,2 +1,13 @@
 #lang racket
 
+(require 2htdp/batch-io)
+
+(define (calc-paper-needed l w h)
+  (let ([side-areas (list (* l w) (* w h) (* h l))])
+    (+ (apply min side-areas)
+       (* 2 (apply + side-areas)))))
+
+(let ([package-dimensions (map (lambda (dims) (map string->number (string-split dims "x")))
+                               (string-split (read-file "2-input.txt")))])
+  (print package-dimensions)
+  (apply + (map (lambda (dims) (apply calc-paper-needed dims)) package-dimensions)))
