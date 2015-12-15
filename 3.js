b702ebd675353d1c3db0e348c9b494058fce1f5f
Nicolas Artman <nicolasartman@gmail.com>
2015-12-14 22:07:09 -0800

    Add day 3 part b solution

--- 3.js
+++ 3.js
@@ -1,5 +1,6 @@
 import * as fs from "fs"
 import cond from "fj-cond"
+import {unique} from "lodash"
 
 const data = fs.readFileSync("3-input.txt").toString();
 
@@ -12,6 +13,10 @@ function getMoveResult(startingCoordinates, move) {
     [() => move === '<', () => [startingCoordinates[0] - 1, startingCoordinates[1]]],
     [() => move === '>', () => [startingCoordinates[0] + 1, startingCoordinates[1]]]
   ])();
+}
+
+function isEven(num) {
+  return num % 2 === 0;
 }
 
 let currentCoordinates = [0, 0];
@@ -28,16 +33,39 @@ assertStringEquals(getMoveResult(currentCoordinates, 'v'), [0, -1]);
 assertStringEquals(getMoveResult(currentCoordinates, '<'), [-1, 0]);
 assertStringEquals(getMoveResult(currentCoordinates, '>'), [1, 0]);
 
+// Main computation
+
+
 let visitedHouseCoordinates = new Map();
 
-visitedHouseCoordinates.set(currentCoordinates.toString(), 1);
+visitedHouseCoordinates.set(currentCoordinates.toString(), true);
 
 for (let step of steps) {
   currentCoordinates = getMoveResult(currentCoordinates, step);
-  let presentsAtLocation =
-      visitedHouseCoordinates.get(currentCoordinates.toString()) || 0;
-  visitedHouseCoordinates.set(currentCoordinates.toString(),
-      presentsAtLocation + 1);
+  visitedHouseCoordinates.set(currentCoordinates.toString(), true);
 }
 
 console.log(`number of houses with at least one present: ${visitedHouseCoordinates.size}`);
+
+// part b
+
+let santaVisits = new Map(),
+    roboVisits = new Map(),
+    santaCoordinates = [0,0],
+    roboCoordinates = [0,0];
+
+santaVisits.set(santaCoordinates.toString(), true);
+roboVisits.set(roboCoordinates.toString(), true);
+
+for (var stepNumber = 0; stepNumber < steps.length; stepNumber++) {
+  if (isEven(stepNumber)) {
+    santaCoordinates = getMoveResult(santaCoordinates, steps[stepNumber]);
+    santaVisits.set(santaCoordinates.toString(), true);
+  } else {
+    roboCoordinates = getMoveResult(roboCoordinates, steps[stepNumber]);
+    roboVisits.set(roboCoordinates.toString(), true);
+  }
+}
+
+console.log('Houses covered by either santa or robosanta: ' +
+    unique(Array.from(santaVisits.keys()).concat(Array.from(roboVisits.keys()))).length);
